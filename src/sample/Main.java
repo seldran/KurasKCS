6538947d62422e8c5ce739988750a3956b997060
seldran <paularbas@yahoo.com>
2017-08-03 20:12:22 +0300

    update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../View/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Fuel Consumption calculator");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
